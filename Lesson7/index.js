ef75655d98229931f739e38007bdf1b8e20ca8e1
xuangia216 <xuangia569@gmail.com>
2023-05-23 23:01:29 +0700

    baitaptrenlopbuoi7

---
+++ Lesson7/index.js
@@ -0,0 +1,20 @@
+
+//let userName = prompt("Nhap ten cua ban:");
+//console.log(`Ten cua ban la ${userName}`); 
+
+//let userName = prompt ("Nhap ten cua ban:");
+//console.log(userName.toUpperCase(userName));
+
+//let userFirstName =prompt("Nhap ho cua ban");
+//let userLastName =prompt("Nhap ten cua ban:");
+//alert(`Hello ${userFirstName} ${userLastName}`);
+
+//let firstNumber=prompt("Nhap so dau tien:");
+//let secondNumber=prompt("Nhap so thu hai:");
+//let tong = Number(firstNumber)+ Number(secondNumber);
+//alert(`Tong cua hai so ${tong}`);
+
+//let addNumber = prompt("Nhap so can tinh:");
+//let noResult=Number(addNumber/2);
+//alert(`Ket qua phep chia ${Math.floor(noResult)} so du ${Math.floor(noResult%2)}`);
+
